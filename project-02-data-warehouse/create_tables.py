a42bdbf1de496d4c8486dbbf5027e5aa9fa7dd96
unknown <ntnk.qse13@gmail.com>
2023-08-27 19:44:17 +0700

    Add docstrings for all the functions in py files

--- project-02-data-warehouse/create_tables.py
+++ project-02-data-warehouse/create_tables.py
@@ -2,29 +2,43 @@ import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-
 def drop_tables(cur, conn):
+    '''
+    Drop all of tables in data warehouse including staging, dim, fact tables.
+    '''
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    '''
+    Create all of tables including staging, dim, fact tables.
+    '''
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    '''
+    Drop tables if exist and create the new one in Sparkify data warehouse. 
+    '''
+
+    # Get the params of the created redshift cluster
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    #Connect to the Redshift Cluster
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
+    # Drop tables if exist
     drop_tables(cur, conn)
+    # Create tables
     create_tables(cur, conn)
 
+    # Close connection to the Redshift Cluster
     conn.close()
 
 
--- project-02-data-warehouse/etl.py
+++ project-02-data-warehouse/etl.py
@@ -4,27 +4,42 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    '''
+    Load data into Redshift staging tables including events and songs from S3
+    '''
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    '''
+    Insert data into dim, fact tables from staging tables in Redshift
+    '''g
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    '''
+    Load data from S3 into staging tables and insert data to dim, fact tables in the Redshift
+    '''
+
+    # Get the params of the created redshift cluster
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # Connect to the Redshift Cluster
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
+    # Load data into Redshift staging tables including events and songs from S3
     load_staging_tables(cur, conn)
+    # Insert data into dim, fact tables from staing tables in Redshift
     insert_tables(cur, conn)
 
+    # Close connection to the Redshift Cluster
     conn.close()
 
 
